62e388e438cfcd7b1699a6d2d71aa555028550ae
Niridya <jessie.gueguen@gmail.com>
2023-09-09 16:59:25 +0200

    Optimisation

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,30 +1,37 @@
-const { InteractionType } = require("discord.js");
-
 module.exports = {
     name: "interactionCreate",
     async execute(client, interaction) {
         if (!interaction.guild) return;
 
-        if (interaction.type == InteractionType.ApplicationCommand) {
+        if (interaction.isCommand()) {
             const command = client.commands.get(interaction.commandName);
             if (!command) return;
 
-            // check permissions
+            // vérification des permissions
             if (command.permissions) {
                 if (command.botOwnerOnly) {
-                    if (!client.config.owners.includes(interaction.user.id)) return interaction.reply({ content: `❌ **Vous devez être le propriétaire du bot pour exécuter cette commande.**`, ephemeral: true }).catch(() => {});
-                }
+                    if (!client.config.owners.includes(interaction.user.id)) return interaction.reply({
+                        content: `❌ **Vous devez être le propriétaire du bot pour exécuter cette commande.**`,
+                        ephemeral: true
+                    });
+                };
 
                 if (command.guildOwnerOnly) {
-                    if (interaction.member.guild.ownerId != interaction.user.id && !client.config.owners.includes(interaction.user.id)) return interaction.reply({ content: `❌ **Vous devez être le propriétaire du serveur pour exécuter cette commande.**`, ephemeral: true }).catch(() => {});
-                }
+                    if (interaction.member.guild.ownerId != interaction.user.id && !client.config.owners.includes(interaction.user.id)) return interaction.reply({
+                        content: `❌ **Vous devez être le propriétaire du serveur pour exécuter cette commande.**`,
+                        ephemeral: true
+                    });
+                };
 
                 const authorPerms = interaction.guild.channels.cache.get(interaction.channelId).permissionsFor(interaction.user);
-                if ((!authorPerms || !authorPerms.has(command.permissions)) && !client.config.owners.includes(interaction.user.id)) return interaction.reply({ content: `❌ **Vous n'avez pas les permissions nécessaires pour exécuter cette commande.**`, ephemeral: true }).catch(() => {});
-            }
+                if ((!authorPerms || !authorPerms.has(command.permissions)) && !client.config.owners.includes(interaction.user.id)) return interaction.reply({
+                    content: `❌ **Vous n'avez pas les permissions nécessaires pour exécuter cette commande.**`,
+                    ephemeral: true
+                });
+            };
 
             command.executeSlash(client, interaction);
             console.log(`[CMD-S]`.brightBlue + ` ${interaction.guild.name} | ${interaction.channel.name} | ${interaction.user.tag} | ${command.name}`);
-        }
+        };
     }
 }
