5ea3a040d0b23f5fa4cc857d3360ab0dba9eea9e
Niridya <jessie.gueguen@gmail.com>
2025-02-16 23:04:42 +0100

    Update interactionCreate.js
    Nouvelle syntaxe pour ephemeral

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -12,21 +12,21 @@ module.exports = {
                 if (command.botOwnerOnly) {
                     if (!client.config.owners.includes(interaction.user.id)) return interaction.reply({
                         content: `❌ **Vous devez être le propriétaire du bot pour exécuter cette commande.**`,
-                        ephemeral: true
+                        flags: ["Ephemeral"]
                     });
                 };
 
                 if (command.guildOwnerOnly) {
                     if (interaction.member.guild.ownerId != interaction.user.id && !client.config.owners.includes(interaction.user.id)) return interaction.reply({
                         content: `❌ **Vous devez être le propriétaire du serveur pour exécuter cette commande.**`,
-                        ephemeral: true
+                        flags: ["Ephemeral"]
                     });
                 };
 
                 const authorPerms = interaction.channel.permissionsFor(interaction.user) || interaction.member.permissions;
                 if (!authorPerms.has(command.permissions) && !client.config.owners.includes(interaction.user.id)) return interaction.reply({
                     content: `❌ **Vous n'avez pas les permissions nécessaires pour exécuter cette commande.**`,
-                    ephemeral: true
+                    flags: ["Ephemeral"]
                 });
             };
 
