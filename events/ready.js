ceef1a256be135a8e79d5911ac386776359c6f20
wanwankinkeeeeee <wanwankinkee@gmail.com>
2025-11-12 20:44:33 +0100

    Update ready.js (#5)
    * Update ready.js
    
    Changer le nom de l'event en clientReady

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,9 @@
 module.exports = {
-    name: "ready",
+    name: "clientReady",
     once: true,
     async execute(client) {
         console.log(`[READY] ${client.user.tag} (${client.user.id}) est prêt | ${client.guilds.cache.size.toLocaleString('fr-FR')} serveurs | ${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0).toLocaleString('fr-FR')} utilisateurs`.green);
     }
+
 }
+
