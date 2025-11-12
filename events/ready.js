465b703dc09c1900efb7213cd1673311021370b8
wanwankinkeeeeee <wanwankinkee@gmail.com>
2025-11-12 20:43:26 +0100

    Update ready.js
    update

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,9 @@
 module.exports = {
-    name: "Clientready",
+    name: "clientReady",
     once: true,
     async execute(client) {
         console.log(`[READY] ${client.user.tag} (${client.user.id}) est prêt | ${client.guilds.cache.size.toLocaleString('fr-FR')} serveurs | ${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0).toLocaleString('fr-FR')} utilisateurs`.green);
     }
 
 }
+
